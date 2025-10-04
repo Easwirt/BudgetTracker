4dc5be862fb97c6fac508bfc9bb762480c2c8ce8
timpimlim <timpimlim2@gmail.com>
2025-10-04 13:56:16 +0200

    Add Feature/TransactionEndpoint

--- backend/budgettracker/src/main/java/com/budget/tracker/budgettracker/controllers/AccountController.java
+++ backend/budgettracker/src/main/java/com/budget/tracker/budgettracker/controllers/AccountController.java
@@ -1,8 +1,11 @@
 package com.budget.tracker.budgettracker.controllers;
 
 import com.budget.tracker.budgettracker.dto.CreateAccountRequest;
+import com.budget.tracker.budgettracker.dto.CreateTransactionRequest;
 import com.budget.tracker.budgettracker.persistance.model.Account;
+import com.budget.tracker.budgettracker.persistance.model.Transaction;
 import com.budget.tracker.budgettracker.service.AccountService;
+import com.budget.tracker.budgettracker.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,9 +19,18 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
     
+    @Autowired
+    private TransactionService transactionService;
+    
     @PostMapping("/account")
     public ResponseEntity<Account> createAccount(@Valid @RequestBody CreateAccountRequest request) {
         Account createdAccount = accountService.createAccount(request);
         return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
+    }
+    
+    @PostMapping("/transactions")
+    public ResponseEntity<Transaction> createTransaction(@Valid @RequestBody CreateTransactionRequest request) {
+        Transaction createdTransaction = transactionService.createTransaction(request);
+        return new ResponseEntity<>(createdTransaction, HttpStatus.CREATED);
     }
 }
---
+++ backend/budgettracker/src/main/java/com/budget/tracker/budgettracker/persistance/model/TransactionType.java
@@ -0,0 +1,6 @@
+package com.budget.tracker.budgettracker.persistance.model;
+
+public enum TransactionType {
+    INCOME,
+    EXPENSE
+}
